893fc121c718729af176acf3fba5ecad7f382fa9
Renato  Avellar <ravellar.machado@gmail.com>
2021-01-07 15:06:05 -0300

    create steps header

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,12 @@
+import StepsHeader from './StepsHeader';
 import './styles.css';
 
 
 function Orders(){
   return(
-    <h1>Orders</h1>
+    <div className="orders-container">
+      <StepsHeader />
+    </div>
   );
 }
 
